dc99bd086ea6c665e65a382eefa7c0796aa3d4c0
Abolfazl-ghasmy <abolfazll606060@gmail.com>
2025-11-08 23:21:08 -0800

    Add files via upload

---
+++ ex408181.php
@@ -0,0 +1,41 @@
+<?php
+
+abstract class Shape {
+    const UNIT = "cm";
+    
+    abstract public function area();
+}
+
+class Rectangle extends Shape {
+    private $width;
+    private $height;
+    
+    public function __construct($width, $height) {
+        $this->width = $width;
+        $this->height = $height;
+    }
+    
+    public function area() {
+        return $this->width * $this->height;
+    }
+}
+
+class Circle extends Shape {
+    private $radius;
+    
+    public function __construct($radius) {
+        $this->radius = $radius;
+    }
+    
+    public function area() {
+        return pi() * pow($this->radius, 2);
+    }
+}
+
+$rectangle = new Rectangle(10, 5);
+$circle = new Circle(5);
+
+echo "Rectangle area: " . $rectangle->area() . " " . Shape::UNIT . "\n";
+echo "Circle area: " . round($circle->area(), 1) . " " . Shape::UNIT . "\n";
+
+?>
